870aa3f7be4f66b6d86a1778f2fcf3962716e744
Justin Snider <66387661+sniderjustin@users.noreply.github.com>
2021-09-26 09:14:14 -0700

    Fix decay_rate variable
    should have been all caps

--- train_pytorch.py
+++ train_pytorch.py
@@ -182,6 +182,8 @@ for epoch in range(1, NUM_EPOCHS+1):
         loss_sum = 0
 
         if epoch > 1:
+            # Comment out by Justin
+            # LEARNING_RATE *= decay_rate ** (global_step // decay_steps)
             LEARNING_RATE *= decay_rate ** (global_step // decay_steps)
             if LEARNING_RATE > LEARNING_RATE_MIN:
                 print("NEW LEARNING RATE:", LEARNING_RATE)
